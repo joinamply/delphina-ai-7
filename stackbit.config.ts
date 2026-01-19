bb95e2c46057e90af53746f061a5f38b1542464e
Alberto de Olivira <betodeoliveira@outlook.com>
2026-01-19 19:54:31 -0300

    feat: Define Stackbit Page model in configuration and create initial index page content.

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -8,8 +8,16 @@ export default defineStackbitConfig({
   contentSources: [
     new GitContentSource({
       rootPath: __dirname,
-      contentDirs: ['content'],
-      models: [],
+      contentDirs: ["content"],
+      models: [
+        {
+          name: "Page",
+          type: "page",
+          urlPath: "/{slug}",
+          filePath: "content/pages/{slug}.json",
+          fields: [{ name: "title", type: "string", required: true }]
+        }
+      ],
       assetsConfig: {
         referenceType: 'static',
         staticDir: 'public',
